b0dc76c62d4189fa814f3711b0bfa41abc80ad39
Marc McIntosh <email@marcmcintosh.ninja>
2016-12-29 02:12:49 +0000

    modified: task.js

--- projects/quote-machine/task.js
+++ projects/quote-machine/task.js
@@ -1,6 +1,9 @@
 module.exports = {
-  title: '',
-  task: '',
-  src: '',
-  stroies: [],
+  title: 'Build a Random Quote Machine',
+  challenge: 'Build a CodePen.io app that is functionally similar to this: ',
+  example: 'https://codepen.io/FreeCodeCamp/full/NNvBQW/',
+  stroies: [
+    'I can view a tribute page with an image and text.',
+    'I can click on a link that will take me to an external website with further information on the topic.',
+  ],
 };
