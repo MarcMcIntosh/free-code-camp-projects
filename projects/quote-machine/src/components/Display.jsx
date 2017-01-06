ecffae2160b029c35fff63f64acc707030eb5b2d
Marc McIntosh <email@marcmcintosh.ninja>
2017-01-06 12:38:49 +0000

    modified: src/components/Display.jsx

--- projects/quote-machine/src/components/Display.jsx
+++ projects/quote-machine/src/components/Display.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchQuote } from '../actions';
 
-const Display = props => (
-  <div className={props.classed}>Content</div>
-);
+class Display extends Component {
+  componentWillMount() {
+    if (!this.props.author || this.props.quote) {
+      this.props.fetchQuote();
+    }
+  }
+
+  render() {
+    const cn = this.props.classed;
+    if (this.props.isLoading) {
+      return (<div className={`${cn} loading`}>Loading</div>);
+    }
+    if (this.props.error) {
+      return (<div className={`${cn} error`}>{this.props.error}</div>);
+    }
+    return (
+      <div className={cn}>
+        <q>{this.props.quote}</q>
+        <bdo dir="rtl">-- {this.props.author}</bdo>
+      </div>
+    );
+  }
+}
 
 Display.propTypes = {
-  classed: React.PropTypes.string,
+  classed: PropTypes.string,
+  author: PropTypes.string,
+  quote: PropTypes.string,
+  fetchQuote: PropTypes.func,
+  error: PropTypes.string,
+  isLoading: PropTypes.bool,
 };
 
-export default Display;
+Display.defaultProps = {
+  classed: 'qm__display',
+};
+
+const mapStateToProps = state => ({
+  ...state.author,
+  ...state.quote,
+  ...state.error,
+  ...state.isFetching,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchQuote: () => dispatch(fetchQuote()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Display);
