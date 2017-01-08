ec505f9b4f3a7e6df5c98f1e449d32ba2cf20873
Marc McIntosh <email@marcmcintosh.ninja>
2017-01-08 08:22:15 +0000

    modified: package.json new file: src/actions.js new file: src/reducer.js new file: src/store.js modified: task.js

---
+++ projects/wikipedia-viewer/src/reducer.js
@@ -0,0 +1,13 @@
+
+const DEFAULT_STATE = {
+  isFetching: false,
+  results: [],
+};
+
+function reducer(state = DEFAULT_STATE, action) {
+  switch (action.type) {
+    default: return state;
+  }
+}
+
+export default reducer;
--- projects/wikipedia-viewer/task.js
+++ projects/wikipedia-viewer/task.js
@@ -1,7 +1,11 @@
+
 module.exports = {
-  title: '',
-  task: '',
-  challenge: '',
-  example: '',
-  stroies: [],
+  challenge: 'https://www.freecodecamp.com/challenges/build-a-wikipedia-viewer',
+  title: 'Build a Wikipedia Viewer',
+  objective: 'Build a CodePen.io app that is functionally similar to this',
+  example: 'https://codepen.io/FreeCodeCamp/full/wGqEga/',
+  stroies: [
+    'I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.',
+    'I can click a button to see a random Wikipedia entry.',
+  ],
 };
