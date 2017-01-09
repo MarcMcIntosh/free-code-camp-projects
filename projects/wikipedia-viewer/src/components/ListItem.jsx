3000b4d497ba951fbe29aee8a1bc57beccb376b9
Marc McIntosh <email@marcmcintosh.ninja>
2017-01-09 10:27:46 +0000

    modified: src/components/ListItem.jsx

--- projects/wikipedia-viewer/src/components/ListItem.jsx
+++ projects/wikipedia-viewer/src/components/ListItem.jsx
@@ -1,18 +1,16 @@
 import React, { PropTypes } from 'react';
 
 const ListItem = (props) => {
-  const { title, extract, pageid, ...args } = props;
-  return (
-    <div {...args}>
-      <h1><a
-        href={`https://en.wikipedia.org/?curid=${pageid}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        tabIndex="0"
-      >{title}</a></h1>
-      <p>{extract}</p>
-    </div>
-  );
+  const { pageid, title, extract, ...args } = props;
+  return (<div {...args}>
+    <h3><a
+      href={`https://en.wikipedia.org/?curid=${pageid}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      tabIndex="0"
+    >{title}</a></h3>
+    <p>{extract}</p>
+  </div>);
 };
 
 ListItem.propTypes = {
