974133483f62e038c1041138a2fc1331cce28e7a
Marc McIntosh <email@marcmcintosh.ninja>
2017-01-12 06:39:00 +0000

    modified: task.js

--- projects/calculator/task.js
+++ projects/calculator/task.js
@@ -1,7 +1,11 @@
 module.exports = {
-  title: '',
-  objective: '',
-  challenge: '',
-  example: '',
-  stroies: [],
+  title: 'Build a JavaScript Calculator',
+  objective: 'Build a CodePen.io app that is functionally similar to this:',
+  challenge: 'https://www.freecodecamp.com/challenges/build-a-javascript-calculator',
+  example: 'https://codepen.io/FreeCodeCamp/full/rLJZrA/',
+  stroies: [
+    'I can add, subtract, multiply and divide two numbers.',
+    'I can clear the input field with a clear button.',
+    'I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.',
+  ],
 };
