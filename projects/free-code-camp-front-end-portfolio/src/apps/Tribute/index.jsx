74e73a308c73f03be4f7b34e036b6bde74578f91
Marc McIntosh <email@marcmcintosh.ninja>
2017-01-29 14:55:41 +0000

    modified: lib/taskbar new file: src/apps/Tribute/Task.jsx renamed: src/apps/Tribute.jsx -> src/apps/Tribute/index.jsx

--- projects/free-code-camp-front-end-portfolio/src/apps/Tribute/index.jsx
+++ projects/free-code-camp-front-end-portfolio/src/apps/Tribute/index.jsx
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react';
 import {
-  task,
   config,
   Quotes,
   Image,
   Title,
 } from '../../lib/tribute-page';
 
-const Tribute = () => (<div />);
+const Tribute = () => (
+  <TaskBar
+  <div />);
