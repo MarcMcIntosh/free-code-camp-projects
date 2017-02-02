7ec3d1d3c99478cf072b0914791fbb743e8174a6
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-02 19:06:58 +0000

    modified: task.js

--- projects/local-weather/task.js
+++ projects/local-weather/task.js
@@ -4,7 +4,7 @@ module.exports = {
   challenge: 'https://www.freecodecamp.com/challenges/show-the-local-weather',
   objective: 'Build a CodePen.io app that is functionally similar to this:',
   example: 'http://codepen.io/FreeCodeCamp/full/bELRjV',
-  stroies: [
+  stories: [
     'I can see the weather in my current location.',
     'I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.',
     'I can push a button to toggle between Fahrenheit and Celsius.',
