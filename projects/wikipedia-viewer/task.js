f334ad5c4316f423c1a7aedd1974927c2936aa56
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-03 19:05:10 +0000

    modified: task.js

--- projects/wikipedia-viewer/task.js
+++ projects/wikipedia-viewer/task.js
@@ -4,7 +4,7 @@ module.exports = {
   title: 'Build a Wikipedia Viewer',
   objective: 'Build a CodePen.io app that is functionally similar to this',
   example: 'https://codepen.io/FreeCodeCamp/full/wGqEga/',
-  stroies: [
+  stories: [
     'I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.',
     'I can click a button to see a random Wikipedia entry.',
   ],
