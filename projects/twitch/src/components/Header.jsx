6533aa2b8fd0925d40d498a8f0344bd7d7331dda
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-03 23:53:47 +0000

    modified: src/components/Header.jsx

--- projects/twitch/src/components/Header.jsx
+++ projects/twitch/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react';
-import { connect } from 'redux';
+import { connect } from 'react-redux';
 import { toggleDisplay } from '../actions';
 import { Filter } from './Filter';
 
