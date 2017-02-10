c8788ba6981c5d7bc0b2f7587f53aa35475cfbab
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-10 10:19:22 +0000

    modified: task.js

--- projects/tic-tac-toe/task.js
+++ projects/tic-tac-toe/task.js
@@ -3,7 +3,7 @@ module.exports = {
   objective: 'Build an app that is functionally similar to this:',
   challenge: 'https://www.freecodecamp.com/challenges/build-a-tic-tac-toe-game',
   example: 'https://codepen.io/FreeCodeCamp/full/KzXQgy/',
-  stroies: [
+  stories: [
     'I can play a game of Tic Tac Toe with the computer.',
     'My game will reset as soon as it\'s over so I can play again.',
     'I can choose whether I want to play as X or O.',
