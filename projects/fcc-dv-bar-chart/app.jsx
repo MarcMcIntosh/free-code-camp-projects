78ce9407c8ee9d0b7cbd94ef9871614ac589e4d4
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-13 18:15:15 +0000

    modified: app.jsx modified: webpack.config.js

--- projects/fcc-dv-bar-chart/app.jsx
+++ projects/fcc-dv-bar-chart/app.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import { render } from 'react-dom';
 import App from './index';
-// import './src/styles/main.scss';
-import './src/styles/test.css';
+import './src/styles/main.scss';
+// import './src/styles/test.css';
 
 const Test = () => (
   <App className="foo-bar">Hello World</App>
