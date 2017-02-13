2f52a0caa1638f9e4266c2927de65ca88271c9be
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-13 17:42:22 +0000

    modified: app.jsx deleted: npm-debug.log modified: package.json new file: src/styles/test.css modified: webpack.config.js

--- projects/fcc-dv-heat-map/app.jsx
+++ projects/fcc-dv-heat-map/app.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import { render } from 'react-dom';
 import App from './index';
-
+// import './src/styles/main.scss';
+import './src/styles/test.css';
 
 const Test = () => (
-  <App>Hello World</App>
+  <App className="foo-bar">Hello World</App>
 );
 
 render(<Test />, document.getElementById('App'));
