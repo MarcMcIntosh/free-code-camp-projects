dc2939fcfb80fb87b5c4d009bbbf2ebda4f4b3c8
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-13 17:08:39 +0000

    modified: app.jsx new file: npm-debug.log modified: package.json modified: src/styles/main.scss modified: webpack.config.js copied: webpack.config.js -> webpack.config0.js

--- projects/fcc-dv-game-of-life/app.jsx
+++ projects/fcc-dv-game-of-life/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import { render } from 'react-dom';
 import App from './index';
-import './src/styles/main.scss';
+
 
 const Test = () => (
   <App>Hello World</App>
