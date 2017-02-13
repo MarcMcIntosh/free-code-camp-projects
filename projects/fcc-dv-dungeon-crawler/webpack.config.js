06fb0ebcf482ddc0282750cdfcae8665d7c22928
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-13 18:15:15 +0000

    modified: app.jsx modified: webpack.config.js

--- projects/fcc-dv-dungeon-crawler/webpack.config.js
+++ projects/fcc-dv-dungeon-crawler/webpack.config.js
@@ -3,11 +3,30 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const IS_DEV = process.env.NODE_ENV === 'development';
 const IS_PROD = process.env.NODE_ENV === 'production';
 const buildDir = IS_PROD ? 'build' : 'dev';
 const OUT_PATH = path.resolve(__dirname, buildDir);
+
+const CSS_LOADER_CONFIG = [{
+  loader: 'css-loader',
+  options: { sourceMap: IS_DEV },
+}, {
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => [autoprefixer],
+  },
+}, {
+  loader: 'sass-loader',
+  options: {
+    modules: true,
+    includePaths: [path.resolve(__dirname, 'node_modules')],
+    sourceMap: IS_DEV,
+  },
+  // options: { sourceMap: true },
+}];
 
 const rules = [
   {
@@ -42,11 +61,25 @@ const rules = [
     }],
   },
   {
-    test: /\.css$/,
+    test: /\.scss$/,
     use: ExtractTextPlugin.extract({
       fallback: 'style-loader',
-      use: 'css-loader',
+      use: CSS_LOADER_CONFIG,
     }),
+  },
+  {
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'file-loader',
+  }, {
+    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+  }, {
+    test: /\.gif/,
+    loader: 'url-loader?limit=10000&mimetype=image/gif',
+  }, { test: /\.jpg/,
+    loader: 'url-loader?limit=10000&mimetype=image/jpg',
+  }, { test: /\.png/,
+    loader: 'url-loader?limit=10000&mimetype=image/png',
   },
 ];
 
