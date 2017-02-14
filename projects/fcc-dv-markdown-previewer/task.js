9199f1c2e1b3a3366c81f9ce5f44a6d841512af7
Marc McIntosh <email@marcmcintosh.ninja>
2017-02-14 10:30:22 +0000

    modified: package.json new file: task.js

---
+++ projects/fcc-dv-markdown-previewer/task.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Build a Markdown Previewer',
+  challenge: 'https://www.freecodecamp.com/challenges/build-a-markdown-previewer',
+  objective: 'Build a CodePen.io app that is functionally similar to this:',
+  example: 'https://codepen.io/FreeCodeCamp/full/JXrLLE/',
+  stroies: [
+    'I can type GitHub-flavored Markdown into a text area.',
+    'I can see a preview of the output of my markdown that is updated as I type.',
+  ],
+};
