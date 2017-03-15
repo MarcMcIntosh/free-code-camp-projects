043437f88f81e21a9bbb0fdecde94aae4cd04f38
Marc McIntosh <email@marcmcintosh.ninja>
2017-03-15 14:03:57 +0000

    modified: _style.scss modified: package.json deleted: src/components/Game0.jsx deleted: src/components/PlayPause.jsx deleted: src/components/Reset.jsx deleted: src/components/ResetButon.jsx deleted: src/components/SVG/index.jsx deleted: src/components/Settings.jsx deleted: src/components/Table.jsx

--- projects/fcc-dv-game-of-life/src/components/Reset.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from './Button';
-
-const Reset = props => (<Button
-  className="reset"
-  title="reset" {...props}
->Reset</Button>);
-
-export default Reset;
