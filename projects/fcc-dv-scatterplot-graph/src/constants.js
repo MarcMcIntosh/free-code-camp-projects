ffe42c1f126cc48b78639a7a06bc91ca0ceede07
Marc McIntosh <email@marcmcintosh.ninja>
2017-04-30 19:47:02 +0100

    modified: package.json new file: src/constants.js new file: src/data/cyclist-data.json new file: src/genGraph.js modified: task.js

---
+++ projects/fcc-dv-scatterplot-graph/src/constants.js
@@ -0,0 +1,6 @@
+export const SRC_DATA = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+
+export const MAIN_TITLE = 'Doping in Professional Bicycle Racing';
+
+export const SUB_TITLE = '35 Fastest times up Alpe d\'Huez';
+export const SCOPE = 'Normalized to 13.8km distance';
--- projects/fcc-dv-scatterplot-graph/task.js
+++ projects/fcc-dv-scatterplot-graph/task.js
@@ -1,7 +1,11 @@
 module.exports = {
-  title: '',
-  challenge: '',
-  objective: '',
-  example: '',
-  stroies: [],
+  title: 'Visualize Data with a Scatterplot Graph',
+  challenge: 'https://www.freecodecamp.com/challenges/visualize-data-with-a-scatterplot-graph',
+  objective: 'Build a CodePen.io app that is functionally similar to this:',
+  example: 'https://codepen.io/FreeCodeCamp/full/ONxvaa/',
+  stroies: [
+    'I can see performance time visualized in a scatterplot graph.',
+    'I can mouse over a plot to see a tooltip with additional details.',
+    'Here\'s a dataset you can use to build this: https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json',
+  ],
 };
