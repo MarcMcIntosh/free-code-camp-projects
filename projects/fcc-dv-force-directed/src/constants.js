44d58bd119082f6099a386bede8d945d73ebba09
Marc McIntosh <email@marcmcintosh.ninja>
2017-05-15 15:04:51 +0100

    new file: src/constants.js modified: task.js

---
+++ projects/fcc-dv-force-directed/src/constants.js
@@ -0,0 +1,3 @@
+export const DATA_SRC = 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json';
+
+export const IMG_SRC = 'https://www.flag-sprites.com';
--- projects/fcc-dv-force-directed/task.js
+++ projects/fcc-dv-force-directed/task.js
@@ -1,7 +1,10 @@
 module.exports = {
-  title: '',
-  challenge: '',
-  objective: '',
-  example: '',
-  stroies: [],
+  title: 'Show National Contiguity with a Force Directed Graph',
+  challenge: 'https://www.freecodecamp.com/challenges/show-national-contiguity-with-a-force-directed-graph',
+  objective: 'Build a CodePen.io app that is functionally similar to this:',
+  example: 'https://codepen.io/FreeCodeCamp/full/xVopBo',
+  stroies: [
+    'I can see each country\'s flag on its node.',
+    'I can see a Force-directed Graph that shows which countries share borders.',
+  ],
 };
