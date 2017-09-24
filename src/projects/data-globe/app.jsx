f73697dc65501d3ad546fd6358d9ee9c716b4599
Marc McIntosh <email@marcmcintosh.ninja>
2017-09-24 14:14:18 +0100

    deleted:    src/projects/data-globe/app.jsx

--- src/projects/data-globe/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-import { render } from 'react-dom';
-import App, { DataGlobe } from './index';
-import './styles/test.scss';
-
-const Test = (<App><DataGlobe /></App>);
-
-render(Test, document.getElementById('App'));
