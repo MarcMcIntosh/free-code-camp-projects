d5fb82544903a34de89fc767a687518861fd3af8
Marc McIntosh <email@marcmcintosh.ninja>
2017-10-29 01:30:21 +0000

    got ingredients form working nicley

--- src/projects/recipe-box/components/form/renderIngredient.jsx
+++ src/projects/recipe-box/components/form/renderIngredient.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { fieldPropTypes } from 'redux-form';
 import { func, string } from 'prop-types';
 
+class renderIngrdient extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+  }
+
+  render() {
+    const { input, onClick, type, label, meta: { touched, error } } = this.props;
+    const { classnames } = this.context;
+    const cn = classnames({
+      'recipe-box-form__textfield': true,
+      'recipe-box-form__textfield--upgraded': true,
+      'recipe-box-form__textfield--box': true,
+      'recipe-box-form__textfield--with-trailing-icon': true,
+      'recipe-box-form__textfield--invalid': error || false,
+      'recipe-box-form__textfield--focused': this.state.active,
+    });
+    const ln = classnames('recipe-box-form__line', {
+      'recipe-box-form__line--active': this.state.active,
+    });
+
+    const lbl = classnames('recipe-box-form__label', {
+      'recipe-box-form__label--float-above': input.value || this.state.active,
+    });
+    return (<div className={cn}>
+      <input {...input} type={type} onFocus={() => this.setState({ active: true })} onBlur={() => this.setState({ active: false })} className={classnames('recipe-box-form__input')} />
+      <label htmlFor={input.name} className={lbl}>{label}</label>
+      <i
+        className={classnames('recipe-box-form__icon')}
+        tabIndex="0"
+        role="button"
+        title="Remove ingredient"
+        onClick={onClick}
+      >close</i>
+      <div className={ln} />
+      {touched && error && <span className={classnames('recipe-box-form__help')}>{error}</span>}
+    </div>);
+  }
+}
+
+/*
 const renderIngrdient = ({
   input,
   type,
@@ -29,7 +70,7 @@ const renderIngrdient = ({
   <div className={classnames('recipe-box-form__line')} />
   {touched && error && <span className={classnames('recipe-box-form__help')}>{error}</span>}
 </div>);
-
+*/
 renderIngrdient.propTypes = {
   ...fieldPropTypes,
   type: string.isRequired,
