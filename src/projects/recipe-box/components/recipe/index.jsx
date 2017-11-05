cfbebf095e5e02d92a12f6c334d29da83fe6f7b6
Marc McIntosh <email@marcmcintosh.ninja>
2017-11-05 01:09:01 +0000

    touching up display on recipe-box

--- src/projects/recipe-box/components/recipe/index.jsx
+++ src/projects/recipe-box/components/recipe/index.jsx
@@ -2,30 +2,41 @@ import React from 'react';
 import { func, string, array, shape } from 'prop-types';
 
 const Recipe = ({
-  recipe: { title, image, ingredients, preparation },
+  recipe: { title, image, ingredients, preparation, desc },
   onEdit,
   onDelete,
 }, {
   classnames,
-}) => (<div className={classnames('recipe-box-modal__recipe')}>
-  <section classNames={classnames('recipe-box-modal__primary')}>
-    <h1 className={classnames('recipe-box-modal__title')}>{title}</h1>
-    <h2 className={classnames('recipe-box-modal__subtitle')}>Ingredients</h2>
-    <ul className={classnames('recipe-box-modal__ingredients')}>
-      {ingredients.map((d, i) => {
-        const k = `ingredient:${i}`;
-        return (<li key={k} className={classnames('recipe-box-modal__ingredient')}>{d}</li>);
-      })}
-    </ul>
-    <img src={image} alt="" className={classnames('recipe-box-modal__image')} />
+}) => (<div className={classnames('recipe-box-recipe')}>
+  <div className={classnames('recipe-box-recipe__horizontal-block')}>
+    <section className={classnames('recipe-box-recipe__primary')}>
+      <h1 className={classnames('recipe-box-recipe__title')}>{title}</h1>
+      <h2 className={classnames('recipe-box-recipe__subtitle')}>{desc}</h2>
+
+      {/* <img src={image} alt="" className={classnames('recipe-box-recipe__image')} /> */}
+    </section>
+
+    <div className={classnames('recipe-box-recipe__media')}>
+      <div className={classnames('recipe-box-recipe__image')} style={image ? { backgroundImage: `url(${image})` } : {}} />
+    </div>
+  </div>
+  <table className={classnames('recipe-box-recipe__ingredients')}>
+    <tr><th className={classnames('recipe-box-recipe__heading')}>Ingredients</th></tr>
+
+    {ingredients.map((d, i) => {
+      const k = `ingredient:${i}`;
+      return (<tr key={k}><td className={classnames('recipe-box-recipe__ingredient')}>{d}</td></tr>);
+    })}
+  </table>
+
+  <section className={classnames('recipe-box-recipe__supporting-text')}>
+    <p>{preparation}</p>
+
   </section>
 
-
-  <section className={classnames('recipe-box-modal__preparation')}>{preparation}</section>
-
-  <section className={classnames('recipe-box-modal__actions')}>
-    <button tabIndex="0" className={classnames('recipe-box-modal__action')} onClick={onEdit} title="Edit this recipe">Edit</button>
-    <button tabIndex="0" className={classnames('recipe-box-modal__action')} onClick={onDelete} title="Delete this recipe">Delete</button>
+  <section className={classnames('recipe-box-recipe__actions')}>
+    <button tabIndex="0" className={classnames('recipe-box-recipe__action')} onClick={onEdit} title="Edit this recipe">Edit</button>
+    <button tabIndex="0" className={classnames('recipe-box-recipe__action')} onClick={onDelete} title="Delete this recipe">Delete</button>
   </section>
 </div>);
 
@@ -35,6 +46,7 @@ Recipe.propTypes = {
     image: string,
     ingredients: array,
     preparation: string,
+    desc: string,
   }),
   onEdit: func.isRequired,
   onDelete: func.isRequired,
@@ -44,6 +56,7 @@ Recipe.defaultProps = {
   recipe: {
     title: '',
     image: '',
+    desc: '',
     ingredients: [],
     preparation: '',
   },
