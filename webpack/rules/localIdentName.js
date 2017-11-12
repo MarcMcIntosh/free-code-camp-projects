354965796700e5259648f3e00211193c71f8b9e9
Marc McIntosh <email@marcmcintosh.ninja>
2017-11-12 00:10:01 +0000

    removeing unused webpack rules

--- webpack/rules/localIdentName.js
+++ webpack/rules/localIdentName.js
@@ -1,5 +1,6 @@
 // const localIdentName = '[name]__[local]___[hash:base64:5]';
 
-const localIdentName = ({ prodution = false }) => (prodution ? '[hash:base64]' : '[path][name]__[local]--[hash:base64:5]');
+// const localIdentName = ({ prodution = false }) => (prodution ? '[hash:base64:5]' : '[name]-[local]');
+const localIdentName = ({ prodution = false }) => (prodution ? '[hash:base64]' : '[name]-[local]');
 
 module.exports = localIdentName;
