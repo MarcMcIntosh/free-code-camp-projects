c7a2f41e1c42982d41949095b44bdbf2d83bc9e4
Marc McIntosh <email@marcmcintosh.ninja>
2017-12-05 01:04:41 +0000

    Scatter-plotnow uses time scale for x axis

--- src/projects/scatterplot-graph/scatterplot.js
+++ src/projects/scatterplot-graph/scatterplot.js
@@ -1,13 +1,19 @@
 import { select, event } from 'd3-selection';
 import { axisBottom, axisLeft } from 'd3-axis';
-// import { timeParse } from 'd3-time-format';
-import { scaleLinear, scaleOrdinal, schemeCategory10 } from 'd3-scale';
+import {
+  // timeParse,
+  timeFormat,
+} from 'd3-time-format';
+import { timeSecond } from 'd3-time';
+import {
+  scaleLinear,
+  scaleOrdinal,
+  schemeCategory10,
+  scaleTime,
+} from 'd3-scale';
 import { extent } from 'd3-array';
 import 'd3-transition';
 // import { formatData } from './utils';
-
-// const parseTime = timeParse('%M:%S');
-// const formatTime = timeFormat('%M:%S');
 /*
 function formatData(data) {
   const bestTime = data.map(d => d.Seconds).reduce((a, b) => Math.min(a, b));
@@ -27,10 +33,15 @@ const color = scaleOrdinal(schemeCategory10);
 
 export default function scatterplot(elem, data, classnames) {
   // const data = formatData(data);
+
+  // const parseTime = timeParse(':%S');
+  // const formatTime = timeFormat('%M:%S');
+  // const toTime = s => formatTime(parseTime(s));
+
   const phi = (1 + Math.sqrt(5)) / 2;
   const w = 600 * phi;
   const h = 600;
-  const margins = { top: 10, right: 30, bottom: 60, left: 80 };
+  const margins = { top: 10, right: 40, bottom: 60, left: 40 };
   const svg = select(elem).append('svg')
     .attr('class', classnames('scatterplot__graph'))
     .attr('viewBox', `0 0 ${w} ${h}`)
@@ -38,26 +49,46 @@ export default function scatterplot(elem, data, classnames) {
 
   const width = w - (margins.left + margins.right);
   const height = h - (margins.top + margins.bottom);
-  // const xScale = scaleLinear().rangeRound([0, width]).domain(extent(data, d => d.behind).reverse());
-  const xScale = scaleLinear().rangeRound([0, width]).domain(extent(data, d => d.Seconds).reverse());
-  const xAxis = axisBottom().scale(xScale);
 
-  // const yScale = scaleLinear().range([0, height]).domain([0, data.length + 1]);
-  // .domain(extent(data, d => d.position));
-  const yScale = scaleLinear().range([0, height]).domain(extent(data, d => d.Place));
+  const xScale = scaleTime().rangeRound([0, width]).domain(extent(data, d => new Date(d.Seconds * 1000)).reverse()).nice(timeSecond, 30);
+
+  const xAxis = axisBottom().scale(xScale).tickFormat(timeFormat('%M:%S'));
+
+  const yScale = scaleLinear()
+    .rangeRound([0, height])
+    .domain(extent(data, d => d.Place))
+    // .nice()
+  ;
+
   const yAxis = axisLeft().scale(yScale);
 
   const g = svg.append('g').attr('transform', `translate(${margins.left},${margins.top})`);
 
-  g.append('g').attr('class', classnames('scatterplot__axis')).attr('transform', `translate(0,${height})`).call(xAxis);
+  g.append('g')
+    .attr('class', classnames('scatterplot__axis'))
+    .attr('transform', `translate(0,${height})`)
+    .call(xAxis)
+    .selectAll('text')
+    .attr('dy', '1em');
+
+  // text label for x axis
+  svg.append('text').attr('transform', `translate(${w / 2}, ${h})`).style('text-anchor', 'middle').text('Finish Time');
 
   g.append('g').attr('class', classnames('scatterplot__axis')).call(yAxis);
+
+  // text label for the y axis
+  svg.append('text').attr('transform', 'rotate(-90)')
+    .attr('y', 0)
+    .attr('x', 0 - (h / 2))
+    .attr('dy', '1em')
+    .style('text-anchor', 'middle')
+    .text('Place');
 
   const tooltip = select(elem).append('div').attr('class', classnames('scatterplot__tooltip')).style('opacity', 0);
 
   const circle = g.selectAll('circle').data(data)
     .enter().append('circle')
-    .attr('cx', d => xScale(d.Seconds))
+    .attr('cx', d => xScale(new Date(d.Seconds * 1000)))
     .attr('cy', d => yScale(d.Place))
     .attr('r', 5)
     .style('fill', d => ((d.Doping === '') ? color('No doping allegations') : color('Riders with doping allegations')));
