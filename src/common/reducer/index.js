87ad1775278a277f217197b6717b6dd5c33e51ad
Marc McIntosh <email@marcmcintosh.ninja>
2017-12-21 15:31:54 +0000

    Prepairing to combine project reducers

---
+++ src/common/reducer/index.js
@@ -0,0 +1,48 @@
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+
+import barChart from '../../projects/bar-chart/reducer';
+import calculator from '../../projects/calculator/reducer';
+import dataGlobe from '../../projects/data-globe/reducer';
+import dungeonCrawler from '../../projects/dungeon-crawler/reducer';
+import forceDirected from '../../projects/force-directed/reducer';
+import gameOfLife from '../../projects/game-of-life/reducer';
+import heatMap from '../../projects/heat-map/reducer';
+import leaderBoard from '../../projects/leader-board/reducer';
+import localWeather from '../../projects/local-weather/reducer';
+import markdownPreviewer from '../../projects/markdown-previewer/reducer';
+import pomodoroClock from '../../projects/pomodoro/reducer';
+import quoteMachine from '../../projects/quote-machine/reducer';
+import recipeBox from '../../projects/recipe-box/reducer';
+import scatterPlotGraph from '../../projects/scatterplot-graph/reducer';
+import simon from '../../projects/simon/reducer';
+import ticTacToe from '../../projects/tic-tac-toe/reducer';
+import tributePage from '../../projects/tribute-page/reducer';
+import twitchTv from '../../projects/twitch-tv/reducer';
+import wikipediaViewer from '../../projects/wikipedia-viewer/reducer';
+
+const reducer = combineReducers({
+  barChart,
+  calculator,
+  dataGlobe,
+  dungeonCrawler,
+  forceDirected,
+  gameOfLife,
+  heatMap,
+  leaderBoard,
+  localWeather,
+  markdownPreviewer,
+  pomodoroClock,
+  quoteMachine,
+  recipeBox,
+  form,
+  scatterPlotGraph,
+  simon,
+  ticTacToe,
+  tributePage,
+  twitchTv,
+  wikipediaViewer,
+});
+
+export default reducer;
+export { default as initialState } from './initialState';
--- src/projects/wikipedia-viewer/store.js
+++ src/projects/wikipedia-viewer/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { reducer as formReducer } from 'redux-form';
 import wikipediaViewer from './reducer';
 
-const r = combineReducers({ wikipediaViewer, form: formReducer });
+const r = combineReducers({ wikipediaViewer });
 
 const store = (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') ? createStore(r, compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()), applyMiddleware(thunk)) : createStore(r, applyMiddleware(thunk));
 
